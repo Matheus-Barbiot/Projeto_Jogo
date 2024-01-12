f356a216258e527dc288dec5e5440746f633d3dd
Matheus Barbiot <125671924+Matheus-Barbiot@users.noreply.github.com>
2024-01-11 22:47:30 -0300

    dano no inimigo
    Sistema de dano no inimigo quase desenvolvido, com feedback visual do dano causado no inimigo e etc...
    Falta:
    - animação para dano
    - animação para morte
    
     Provavelmente farei isso nos próximos Commits...
    
    Além disso, eu fiz uma arrumada no código do inimigo, deixando mais organizado e limpo.

--- scripts/inimigo.py
+++ scripts/inimigo.py
@@ -10,13 +10,15 @@ class inimigo(bge.types.KX_PythonComponent):
     ])
 
     def start(self, args):
-        self.character = bge.constraints.getCharacter(self.object)
         self.scene = bge.logic.getCurrentScene()
+        self.character = bge.constraints.getCharacter(self.object)
         
         for n in self.object.children: 
             if "armadura" in n.name:
                 self.armadura = n
-
+        self.hit = self.scene.objects.get("inimigo_ataqueHit")
+        
+        self.pulo = 0
         self.speed = args["Speed"]
         self.distance = args["Distance"]
         self.minDistance = choice([3,4,5,6])
@@ -30,37 +32,55 @@ class inimigo(bge.types.KX_PythonComponent):
     def seguir(self):
         player = self.scene.objects.get("Player")
         if player:
-            # se ele chegar perto do player       
-            # se ele avistar o player
-            if self.object.getDistanceTo(player) < self.distance and not self.object.getDistanceTo(player) < self.minDistance:
-                vetor = mathutils.Vector(player.worldPosition - self.object.worldPosition)
-                self.character.walkDirection = vetor.normalized() * self.speed
-                self.armadura["Andar"] = True
+            if self.object.getDistanceTo(player) < self.distance:
+                if self.object.getDistanceTo(player) > self.minDistance:
+                    vetor = mathutils.Vector(player.worldPosition - self.object.worldPosition)
+                    self.character.walkDirection = vetor.normalized() * self.speed
+                    self.armadura["Andar"] = True
             
-            elif self.object.getDistanceTo(player) < self.minDistance:
-                self.character.walkDirection = mathutils.Vector([0,0,0])
-                self.armadura["Andar"] = False; self.armadura["direcao"] = 0
-                escolha = self.object["escolha"]
-                
-                if escolha == 0:
-                    self.character.walkDirection = mathutils.Vector([0,0,0])
-                    self.armadura["Andar"] = False; self.armadura["direcao"] = 0
-                if self.Ray.hitObject == None:
-                    if escolha == 1:
-                        self.object.applyMovement([self.speed, 0, 0], True)
-                        self.armadura["Andar"] = True; self.armadura["direcao"] = 1
-                        
-                    if escolha == 2:
-                        self.object.applyMovement([-self.speed, 0, 0], True)
-                        self.armadura["Andar"] = True; self.armadura["direcao"] = -1
                 else:
-                    escolha = 0
-            # se ele não ver ou perder o player
+                    escolha = self.object["escolha"]
+                    self.parar()
+                    
+                    if escolha == 3:
+                        self.atacar()
+                            
+                    elif escolha == 1:
+                        self.pulo = 0
+                        self.object["ataqueTemp"] = 0.0
+                        self.armadura["Andar"] = True 
+                        self.armadura["direcao"] = 1
+                        self.object.applyMovement([self.speed, 0, 0], True)
+                        
+                    elif escolha == 2:
+                        self.pulo = 0
+                        self.object["ataqueTemp"] = 0.0
+                        self.armadura["Andar"] = True
+                        self.armadura["direcao"] = -1
+                        self.object.applyMovement([-self.speed, 0, 0], True)
+                  
             else:
-                self.character.walkDirection = mathutils.Vector([0,0,0])
-                self.armadura["Andar"] = False
+                self.parar()
                 return
         
+    def parar(self):
+        self.character.walkDirection = mathutils.Vector([0,0,0])
+        self.armadura["Andar"] = False
+        self.armadura["direcao"] = 0
+    
+    def atacar(self):
+        self.armadura["Andar"] = False
+        self.armadura["direcao"] = 0
+        if self.object["ataqueTemp"] > 0.7:
+            self.hit["ataque"] = True
+            if self.pulo == 0:
+                self.object.applyMovement([0, 2, 0], True)
+                self.character.jump()
+                self.pulo += 1
+            return
         
     def update(self):
+        if self.NearMin.hitObject == None:
+            self.object["escolha"] = 0 
+            self.object["ataqueTemp"] = 0.0
         self.seguir()
